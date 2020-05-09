353981ee945e8e312b67502f3c80dd02c4e59a9f
Keegan Leary <leary.keegan@gmail.com>
2020-05-09 06:11:40 -0700

    ball speed and damping

--- entities/ball.lua
+++ entities/ball.lua
@@ -1,9 +1,10 @@
 local world = require('world')
 
 return function(x_pos, y_pos)
+    local entity_max_speed = 880
+
     local entity = {}
     entity.body = love.physics.newBody(world, x_pos, y_pos, 'dynamic')
-    entity.body:setMass(32)
     entity.body:setLinearVelocity(300, 300)
     entity.shape = love.physics.newCircleShape(0, 0, 10)
     entity.fixture = love.physics.newFixture(entity.body, entity.shape)
@@ -14,6 +15,23 @@ return function(x_pos, y_pos)
     entity.draw = function(self)
         local self_x, self_y = self.body:getWorldCenter()
         love.graphics.circle('fill', self_x, self_y, self.shape:getRadius())
+    end
+
+    entity.update = function(self)
+        local vel_x, vel_y = self.body:getLinearVelocity()
+        local speed = math.abs(vel_x) + math.abs(vel_y)
+
+        local vel_x_is_critical = math.abs(vel_x) > entity_max_speed * 2
+        local vel_y_is_critical = math.abs(vel_y) > entity_max_speed * 2
+
+        if vel_x_is_critical or vel_y_is_critical then
+            self.body:setLinearVelocity(vel_x * 0.75, vel_y * 0.75)
+        end
+        if speed > entity_max_speed then
+            self.body:setLinearDamping(0.1)
+        else
+            self.body:setLinearDamping(0)
+        end
     end
 
     return entity
--- entities/paddle.lua
+++ entities/paddle.lua
@@ -3,9 +3,19 @@ local world = require('world')
 
 
 return function(x_pos, y_pos)
+    local window_width = love.window.getMode()
+
+    local entity_width = 120
+    local entity_height = 20
+    local entity_speed = 600
+
+    local left_boundary = (entity_width / 2) + 2
+    local right_boundary = window_width - (entity_width / 2) - 2
+
+
     local entity = {}
-    entity.body = love.physics.newBody(world, x_pos, y_pos, 'static')
-    entity.shape = love.physics.newRectangleShape(180, 20)
+    entity.body = love.physics.newBody(world, x_pos, y_pos, 'kinematic')
+    entity.shape = love.physics.newRectangleShape(entity_width, entity_height)
     entity.fixture = love.physics.newFixture(entity.body, entity.shape)
     entity.fixture:setUserData(entity)
 
@@ -19,15 +29,13 @@ return function(x_pos, y_pos)
         if input.left and input.right then
             return
         end
-        local self_x, self_y = self.body:getPosition()
-        if input.left then
-          --left side
-            local new_x = math.max(self_x - (400 * dt), 108)
-            self.body:setPosition(new_x, self_y)
-        elseif input.right then
-          --right side
-        local new_x = math.min(self_x + (400 * dt), 700)
-            self.body:setPosition(new_x, self_y)
+        local self_x = self.body:getX()
+        if input.left and self_x > left_boundary then
+            self.body:setLinearVelocity(-entity_speed, 0)
+        elseif input.right and self_x < right_boundary then
+            self.body:setLinearVelocity(entity_speed, 0)
+        else
+            self.body:setLinearVelocity(0, 0)
         end
     end
 
